c214885fcc0205217a612403da1d90c49c5f8bda
Yordi <yorditjordin@gmail.com>
2023-05-01 15:42:03 +0300

    Update date.js

--- modules/date.js
+++ modules/date.js
@@ -3,7 +3,6 @@ import { DateTime } from './luxon.js';
 const Date = () => {
   const date = DateTime.now();
   document.querySelector('.date').innerHTML = date.toLocaleString(
-
     DateTime.DATETIME_MED,
   );
 };
